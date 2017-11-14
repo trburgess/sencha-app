b692f3b02f0040497d47ac7131875781bd3db709
Tom Burgess <tom.burgess@irmsecurity.com>
2017-11-14 20:17:54 +0000

    Refactor `LoginController` to use view model and also tidy up implementation

--- app/view/login/LoginController.js
+++ app/view/login/LoginController.js
@@ -2,32 +2,36 @@ Ext.define('TutorialApp.view.login.LoginController', {
   extend: 'Ext.app.ViewController',
   alias: 'controller.login',
 
-  requires: [
-    'TutorialApp.model.User'
-  ],
+  onLoginClick: function () {
+    var viewModel = this.getViewModel();
+    var foundUser = viewModel.getStore('User').getById(
+      viewModel.get('identity')
+    );
 
-  onLoginClick: function(me) {
-    var form = me.up('form');
-    var userSubmitted = Ext.create('TutorialApp.model.User', form.getValues());
-    var foundUser = Ext.getStore('User').findRecord('username', userSubmitted.get('username'), 0, false, true, true);
-    if(!foundUser) {
-      Ext.Msg.alert("Validation Failed", 'Invalid credentials');
-      return false;
-    } else if (foundUser.get('password') !== userSubmitted.get('password')) {
-      Ext.Msg.alert("Validation Failed", 'Invalid credentials');
+    if (this._validateUser(foundUser, viewModel.get('credential'))) {
+      // Set the localStorage value to true
+      localStorage.setItem('TutorialLoggedIn', true);
+
+      // Remove Login Window
+      this.getView().destroy();
+
+      // Add the main view to the viewport
+      Ext.create({
+        xtype: 'app-main'
+      });
+    }
+  },
+
+  _validateUser: function (foundUser, credential) {
+    if (!foundUser || foundUser.get('credential') !== credential) {
+      this._failedValidationMessage();
       return false;
     }
 
-    // Set the localStorage value to true
-    localStorage.setItem("TutorialLoggedIn", true);
+    return true;
+  },
 
-    // Remove Login Window
-    this.getView().destroy();
-
-    // Add the main view to the viewport
-    Ext.create({
-      xtype: 'app-main'
-    });
-
+  _failedValidationMessage: function () {
+    Ext.Msg.alert('Validation Failed', 'Invalid credentials');
   }
 });
